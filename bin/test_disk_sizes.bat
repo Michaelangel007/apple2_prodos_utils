938b10a581dade9d63b32cf2d6bba7c9932783c8
michaelangel007 <michaelangel007@sharedcraft.com>
2025-01-26 14:29:11 -0800

    Batch file to create disk images of various sizes

---
+++ bin/test_disk_sizes.bat
@@ -0,0 +1,14 @@
+@REM AppleWin currently limits Floppy Disk images to 40 tracks so one will need to mount as a hard drive image.
+prodosfs.exe tracks35.po   init -tracks=35   /TRACKS35
+prodosfs.exe tracks36.po   init -tracks=36   /TRACKS36
+prodosfs.exe tracks37.po   init -tracks=37   /TRACKS37
+prodosfs.exe tracks38.po   init -tracks=38   /TRACKS38
+prodosfs.exe tracks39.po   init -tracks=39   /TRACKS39
+prodosfs.exe tracks40.po   init -tracks=40   /TRACKS40
+prodosfs.exe tracks41.po   init -tracks=41   /TRACKS41
+prodosfs.exe tracks42.po   init -tracks=42   /TRACKS42
+prodosfs.exe tracks43.po   init -tracks=43   /TRACKS43
+prodosfs.exe tracks44.po   init -tracks=44   /TRACKS44
+prodosfs.exe tracks45.po   init -tracks=45   /TRACKS45
+prodosfs.exe tracks200.po  init -tracks=200  /TRACKS200
+prodosfs.exe tracks8192.po init -tracks=8192 /TRACKS8192
